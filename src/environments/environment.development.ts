41466470629c359270de794ac2501b6c9e05c94f
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-12 23:31:49 -0500

    feat: Add configuration environments, and endpoints of bc resident and baseService.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: false,
-  agecareProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+  agecareProviderApiBaseUrl: 'https://agecareapi.com',
+  serverBasePath: 'http://localhost:3000/api/v1',
+  residentsEndpointPath: '/residents',
+  medicationsEndpointPath: '/medications',
+  mentalHealthRecordsEndpointPath: '/mentalHealthRecords',
+  medicalHistoryEndpointPath: '/medicalHistorys'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
   agecareProviderApiBaseUrl: 'https://agecareapi.com',
+  serverBasePath: 'http://localhost:3000/api/v1',
+  residentsEndpointPath: '/residents',
+  medicationsEndpointPath: '/medications',
+  mentalHealthRecordsEndpointPath: '/mentalHealthRecords',
+  medicalHistoryEndpointPath: '/medicalHistorys'
 };
