e7602ad630165b3381a2a970cb7c9cdc567d5fcd
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-12 23:14:22 -0500

    feat: Insert import from MentalHealthRecord

--- src/app/core/pages/resident-care-management/model/resident.entity.ts
+++ src/app/core/pages/resident-care-management/model/resident.entity.ts
@@ -1,5 +1,6 @@
 import {Medication} from './medication.entity';
 import {MedicalHistory} from './medicalHistory.entity';
+import {MentalHealthRecord} from './mentalHealthRecord.entity';
 
 export interface Resident {
   id: number;
