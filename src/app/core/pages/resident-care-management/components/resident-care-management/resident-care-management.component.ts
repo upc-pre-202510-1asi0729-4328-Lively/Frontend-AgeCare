9e57e8e8215eec1befe626e4cfd7fd7efe431471
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-13 00:00:25 -0500

    feat: Add resident-care-management.component

--- src/app/core/pages/resident-care-management/components/resident-care-management/resident-care-management.component.ts
+++ src/app/core/pages/resident-care-management/components/resident-care-management/resident-care-management.component.ts
@@ -1,11 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Resident } from '../../model/resident.entity';
+import { ResidentService } from '../../services/resident.service';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {DatePipe, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-resident-care-management',
-  imports: [],
   templateUrl: './resident-care-management.component.html',
-  styleUrl: './resident-care-management.component.css'
+  imports: [
+    ReactiveFormsModule,
+    NgForOf,
+    DatePipe
+  ],
+  styleUrls: ['./resident-care-management.component.css']
 })
-export class ResidentCareManagementComponent {
+export class ResidentCareManagementComponent implements OnInit {
+  residents: Resident[] = [];
+  residentForm!: FormGroup;
+  editingResidentId: number | null = null;
 
+  constructor(
+    private residentService: ResidentService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.loadResidents();
+    this.initForm();
+  }
+
+  initForm() {
+    this.residentForm = this.fb.group({
+      dni: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      gender: ['', Validators.required],
+    });
+  }
+
+  loadResidents(): void {
+    this.residentService.getAll().subscribe({
+      next: (res) => (this.residents = res),
+      error: (err) => console.error('Failed to load residents', err),
+    });
+  }
+
+  onSubmit(): void {
+    if (this.residentForm.invalid) return;
+
+    const formValue = this.residentForm.value;
+
+    if (this.editingResidentId === null) {
+      this.residentService.create(formValue).subscribe({
+        next: (created) => {
+          this.residents.push(created);
+          this.residentForm.reset();
+        },
+        error: (err) => console.error('Failed to create resident', err)
+      });
+    } else {
+      this.residentService.update(this.editingResidentId, formValue).subscribe({
+        next: () => {
+          const index = this.residents.findIndex(r => r.id === this.editingResidentId);
+          if (index !== -1) {
+            this.residents[index] = { id: this.editingResidentId, ...formValue };
+          }
+          this.cancelEdit();
+        },
+        error: (err) => console.error('Failed to update resident', err)
+      });
+    }
+  }
+
+  editResident(resident: Resident): void {
+    this.editingResidentId = resident.id;
+    this.residentForm.patchValue(resident);
+  }
+
+  deleteResident(id: number): void {
+    this.residentService.delete(id).subscribe({
+      next: () => {
+        this.residents = this.residents.filter((r) => r.id !== id);
+      },
+      error: (err) => console.error('Failed to delete resident', err)
+    });
+  }
+
+  cancelEdit(): void {
+    this.editingResidentId = null;
+    this.residentForm.reset();
+  }
 }
