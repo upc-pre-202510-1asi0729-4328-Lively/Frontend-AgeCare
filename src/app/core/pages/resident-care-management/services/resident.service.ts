494243f18e0b64ffeb09fe4d701ce3273e0aeaa3
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-07-03 19:59:52 -0500

    feat: Add getResidentDetails()

--- src/app/core/pages/resident-care-management/services/resident.service.ts
+++ src/app/core/pages/resident-care-management/services/resident.service.ts
@@ -1,10 +1,9 @@
-// src/app/services/resident.service.ts
-
 import { Injectable } from '@angular/core';
-import { BaseService} from '../../../../public/shared/services/baseService';
-import { Resident} from '../model/resident.entity';
+import { BaseService } from '../../../../public/shared/services/baseService';
+import { Resident } from '../model/resident.entity';
 import { HttpClient } from '@angular/common/http';
-import { environment} from '../../../../../environments/environment';
+import { environment } from '../../../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +12,12 @@ export class ResidentService extends BaseService<Resident> {
   constructor(http: HttpClient) {
     super(http);
     this.resourceEndpoint = environment.residentsEndpointPath;
+  }
+
+  // 🔽 Nuevo método para obtener detalles completos
+  getResidentDetails(id: number): Observable<Resident> {
+    const url = `${this.basePath}${this.resourceEndpoint}/${id}/details`;
+    return this.http.get<Resident>(url, this.httpOptions)
+      .pipe(this.defaultPipeOperators());
   }
 }
