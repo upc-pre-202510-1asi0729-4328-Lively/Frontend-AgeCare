044c41b9d873bc7f0b8c503f3b15ed927c8a8a9e
FatimaAP05 <125381316+FatimaAP05@users.noreply.github.com>
2025-07-09 11:47:22 -0500

    fix: update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { HomeManagementComponent } from './public/pages/home-management/home-man
 import { AppointmentManagementComponent } from './core/pages/appointment-management/components/appointment-management.component';
 import { NotificationsAlertsManagementComponent } from './core/pages/notifications-alerts-management/components/notifications-alerts-management.component';
 import { PaymentManagementDoctorComponent } from './core/pages/payment-management/components/payment-management-doctor/payment-management-doctor.component';
-import { PaymentManagementUserComponent} from './core/pages/payment-management/components/payment-management-user/payment-management-user.component';
+// import { PaymentManagementUserComponent} from './core/pages/payment-management/components/payment-management-user/payment-management-user.component'; // Comentado por error de ruta/no existe
 import { ResidentCareManagementComponent } from './core/pages/resident-care-management/components/resident-care-management/resident-care-management.component';
 import { UserManagementComponent } from './core/pages/user-management/components/user-management/user-management.component';
 
@@ -26,8 +26,9 @@ export const routes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeManagementComponent },
       { path: 'appointments', component: AppointmentManagementComponent },
-      { path: 'notifications-alerts', component: NotificationsAlertsManagementComponent },
-      { path: 'payment', component: PaymentManagementUserComponent },
+      { path: 'notifications-family', component: NotificationsAlertsManagementComponent, data: { view: 'family' } },
+      { path: 'notifications-management', component: NotificationsAlertsManagementComponent, data: { view: 'management' } },
+      // { path: 'payment', component: PaymentManagementUserComponent }, // Comentado por error de ruta/no existe
       { path: 'payment-doctor', component: PaymentManagementDoctorComponent },
       { path: 'resident', component: ResidentCareManagementComponent },
       { path: 'user', component: UserViewComponent }
